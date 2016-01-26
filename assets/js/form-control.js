f95f2e1c8a628efb3ccbf7b24041adde29abb974
Nathan Brown <nkabrown@gmail.com>
2016-01-25 22:55:35 -0500

    Change form transition behavior

--- assets/js/form-control.js
+++ assets/js/form-control.js
@@ -1,8 +1,17 @@
 d3.selectAll('.input-btn')
     .on('click', function() {
-      var form = d3.select('.form-element')
+      var form = d3.select('div.form-element')
           .transition()
           .duration(700)
-          .style('opacity', 0);
-      form.remove();
+          .style('opacity', 0)
+          .transition()
+          .remove();
+      var nextForm = d3.select('div.form-element:nth-child(2)');
+      console.log(nextForm);
+      nextForm.transition().delay(1040)
+          .style('opacity', 0)
+          .transition()
+          .duration(500)
+          .style('opacity', 1)
+          .style('display', 'block');
     });
